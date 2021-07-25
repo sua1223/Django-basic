d0394edce79f466885ad2244a4d69053d5bc4000
sua1223 <70890638+sua1223@users.noreply.github.com>
2021-07-25 21:48:39 +0900

    21/07/25 ADD list

--- tutorial/community/views.py
+++ tutorial/community/views.py
@@ -3,5 +3,17 @@ from community.forms import *
 
 # Create your views here.
 def write(request):
+    if request.method == 'POST':
+        form = Form(request.POST)
+        if form.is_valid:
+            form.save()
+    else:
+        form = Form()
     form = Form()
-    return render(request, 'write.html')
+
+    return render(request, 'write.html', {'form': form})
+
+def list(request):
+    articleList = Article.objects.all()
+    return render(request, 'list.html', {'articleList':articleList})
+    #34
